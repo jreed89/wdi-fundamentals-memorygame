1a0e51f51c1a989b36a9b4a81a3a48d5b8326730
Joshua Daniel <jrdaniel@ncsu.edu>
2016-05-22 20:45:34 -0400

    if else statment

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,22 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+    alert("Sorry, try again.")
+}
+if (cardTwo === cardThree) {
+	alert("Sorry, try again.")
+}
+if (cardOne === cardThree) {
+	alert("Sorry, try again.")
+}
+if (cardOne === cardFour) {
+	alert("Sorry, try again.")
+}
+if (cardOne === cardTwo) {
+	alert("You found a match")
+}
+if (cardThree === cardFour) {
+	alert("You found a match")
+}
